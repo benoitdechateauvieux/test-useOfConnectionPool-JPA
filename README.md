e8767aec696d421d3bb91a67fde83876f3ac49fa
Benoit de Chateauvieux <bdechateauvieux@exoplatform.com>
2015-11-03 12:01:02 +0700

    Check when the connection is released

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Simple unit test that shows that JPA picks a connection from the connection pool as soon as a transaction is started.
+Simple unit test that shows that JPA picks a connection from the connection pool as soon as a transaction is started.   
+
+Conclusions:
+* Creating an Entity Manager doesn't open a connection
+* Starting a transaction opens a connection
+* Committing/Rollbacking a transaction doesn't release the connection
+* Closing an Entity Manager closes the connection
+* Once a connection is associated with an Entity Manager, it is released only by a call to em.close()
--- src/test/java/EntityManagerTest.java
+++ src/test/java/EntityManagerTest.java
@@ -1,66 +1,145 @@
-import com.mchange.v2.c3p0.PoolBackedDataSource;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.Attribute;
+import javax.management.AttributeList;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.management.*;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.lang.management.ManagementFactory;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import com.mchange.v2.c3p0.PoolBackedDataSource;
 
 /**
- * Created by The eXo Platform SAS
- * Author : eXoPlatform
- * exo@exoplatform.com
+ * Created by The eXo Platform SAS Author : eXoPlatform exo@exoplatform.com
  * 9/24/15
  */
 public class EntityManagerTest {
 
-    private EntityManagerFactory emf;
+  private EntityManagerFactory emf;
 
-    @Before
-    public void initEmf() {
-        emf = Persistence.createEntityManagerFactory("exo-pu");
-    }
+  @Before
+  public void initEmf() {
+    emf = Persistence.createEntityManagerFactory("exo-pu");
+  }
 
-    @After
-    public void closeEmf() {
-        emf.close();
-    }
+  @After
+  public void closeEmf() {
+    emf.close();
+  }
 
-    @Test
-    public void createEM_doesnotCreateConnection() throws Exception {
-        //Given
-        //When
-        EntityManager em = emf.createEntityManager();
-        //Then
-        assertThat(getNumBusyConnection(), is(0));
-        em.close();
-    }
+  @Test
+  public void createEM_doesntOpenConnection() throws Exception {
+    // Given
+    // When
+    EntityManager em = emf.createEntityManager();
+    // Then
+    Thread.sleep(2*1000L);
+    assertThat(getNumBusyConnection(), is(0));
+    em.close();
+  }
 
-    @Test
-    public void createOpenTransaction_doesnotCreateConnection() throws Exception {
-        //Given
-        EntityManager em = emf.createEntityManager();
-        //When
-        em.getTransaction().begin();
-        //Then
-        assertThat(getNumBusyConnection(), is(0));
-        em.close();
-    }
+  @Test
+  public void select_doesntImmediatelyReleaseConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    // When
+    Query query = em.createNativeQuery("SELECT 1 FROM INFORMATION_SCHEMA.SYSTEM_USERS");
+    Integer one = (Integer) query.getSingleResult();
+    assertThat(one, is(1));
+    // Then
+    Thread.sleep(2 * 1000L);
+    assertThat(getNumBusyConnection(), is(1));
+    em.close();
+  }
 
-    private int getNumBusyConnection() throws Exception {
-        MBeanServer mbs    = ManagementFactory.getPlatformMBeanServer();
-        ObjectName name    = ObjectName.getInstance("com.mchange.v2.c3p0:type=C3P0Registry");
-        AttributeList list = mbs.getAttributes(name, new String[]{"AllPooledDataSources"});
-        PoolBackedDataSource dataSource = (PoolBackedDataSource) ((java.util.Set)((Attribute)list.get(0)).getValue()).iterator().next();
-        String identityToken = dataSource.getIdentityToken();
-        ObjectName nameDs  = ObjectName.getInstance("com.mchange.v2.c3p0:type=PooledDataSource[" + identityToken + "]");
-        AttributeList numBusylist = mbs.getAttributes(nameDs, new String[]{"numBusyConnectionsAllUsers"});
-        return (Integer)((Attribute)numBusylist.get(0)).getValue();
-    }
+  @Test
+  public void clear_doesntImmediatelyReleaseConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    Query query = em.createNativeQuery("SELECT 1 FROM INFORMATION_SCHEMA.SYSTEM_USERS");
+    Integer one = (Integer) query.getSingleResult();
+    assertThat(one, is(1));
+    // When
+    em.clear();
+    // Then
+    Thread.sleep(2 * 1000L);
+    assertThat(getNumBusyConnection(), is(0));
+    em.close();
+  }
+
+  @Test
+  public void beginTransaction_openConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    // When
+    em.getTransaction().begin();
+    // Then
+    Thread.sleep(2*1000L);
+    assertThat(getNumBusyConnection(), is(1));
+    em.close();
+  }
+
+  @Test
+  public void commitTransaction_doesntCloseConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    assertThat(getNumBusyConnection(), is(1));
+    // When
+    em.getTransaction().commit();
+    // Then
+    Thread.sleep(2*1000L);
+    assertThat(getNumBusyConnection(), is(1));
+    em.close();
+  }
+
+  @Test
+  public void rollbackTransaction_doesntCloseConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    assertThat(getNumBusyConnection(), is(1));
+    // When
+    em.getTransaction().rollback();
+    // Then
+    Thread.sleep(2*1000L);
+    assertThat(getNumBusyConnection(), is(1));
+    em.close();
+  }
+
+  @Test
+  public void closeEM_afterCloseTransaction_closeConnection() throws Exception {
+    // Given
+    EntityManager em = emf.createEntityManager();
+    em.getTransaction().begin();
+    assertThat(getNumBusyConnection(), is(1));
+    em.getTransaction().commit();
+    assertThat(getNumBusyConnection(), is(1));
+    // When
+    em.close();
+    // Then
+    Thread.sleep(2 * 1000L);
+    assertThat(getNumBusyConnection(), is(0));
+  }
+
+  private int getNumBusyConnection() throws Exception {
+    MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+    ObjectName name = ObjectName.getInstance("com.mchange.v2.c3p0:type=C3P0Registry");
+    AttributeList list = mbs.getAttributes(name, new String[] { "AllPooledDataSources" });
+    PoolBackedDataSource dataSource = (PoolBackedDataSource) ((java.util.Set) ((Attribute) list.get(0)).getValue()).iterator()
+                                                                                                                   .next();
+    String identityToken = dataSource.getIdentityToken();
+    ObjectName nameDs = ObjectName.getInstance("com.mchange.v2.c3p0:type=PooledDataSource[" + identityToken + "]");
+    AttributeList numBusylist = mbs.getAttributes(nameDs, new String[] { "numBusyConnectionsAllUsers" });
+    return (Integer) ((Attribute) numBusylist.get(0)).getValue();
+  }
 }
